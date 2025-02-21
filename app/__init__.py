7eaeed2e3f2b1e9cd2627182593c36d7699d5415
danishkodemonkey <humberjini@hotmail.com>
2025-02-21 16:20:21 +0100

    Get rid of disconnect for now, prisma handles it automatically

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,12 @@
 from flask import Flask
 from app.routes import register_blueprints
 from app.db import disconnect_prisma
+import asyncio
 
 
-def create_app():
+def create_app() -> None:
     app = Flask(__name__)
 
     register_blueprints(app)
-
-    @app.teardown_appcontext
-    async def shutdown(exception=None):
-        await disconnect_prisma()
 
     return app
