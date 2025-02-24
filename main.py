a98a87da8efff85963954eb059cd87eb6b8792c4
danishkodemonkey <humberjini@hotmail.com>
2025-02-24 15:52:04 +0100

    Refactor to simply initiate fastAPI for local python main.py init cases

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
 # Entrypoint, app initializer
 
 from app import create_app
-from app.db import PrismaInit
-import asyncio
 
-
-def init_app():
-
-    PrismaInit()
-
-    app = create_app()
-
-    app.run(host="0.0.0.0", port=5000, debug=True)
-
+app = create_app()
 
 if __name__ == "__main__":
-    init_app()
+    app.run(host="0.0.0.0", port=8000, reload=True)
