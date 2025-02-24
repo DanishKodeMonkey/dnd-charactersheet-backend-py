b9bb1a77308172c6f6f2e9423a3cfaf40190513d
danishkodemonkey <humberjini@hotmail.com>
2025-02-24 15:50:57 +0100

    Refactor with two simple connection functions to call in fastAPI init

--- app/db.py
+++ app/db.py
@@ -1,54 +1,17 @@
 import logging
-import asyncio
-from functools import wraps
-from prisma import Prisma, register
+from prisma import Prisma
 
 logging.basicConfig(level=logging.INFO)
 logging.getLogger("prisma").setLevel(logging.DEBUG)
 
-
-def PrismaInit():
-    logging.info("Initiating Prisma...")
-    prisma = Prisma()
-    logging.info("Prisma Initiated")
-    logging.info("Connecting prisma to Database")
-    prisma.connect()
-    logging.info("Prisma connected to database")
-    logging.info("Registering Prisma client...")
-    register(prisma)  # Register operation lifecycle with prisma
-    logging.info("Prisma client registered")
+db = Prisma()
 
 
-""" 
-Below deprecated for now, turns out continous 
-connection/disconnections is expensive and is better to generally just let Prisma handle it 
-"""
-
-""" async def connect_prisma():
-    logging.info("connecting Prisma")
-    await prisma.connect()
-    logging.info("prisma connected")
+async def connect_db():
+    """Connect to Prisma database."""
+    await db.connect()
 
 
-async def disconnect_prisma():
-    logging.info("disconnecting Prisma")
-    await prisma.disconnect()
-    logging.info("prisma disconnected")
-
-
-def prismaQuery(func):
-    Decorator ensures Prisma is connected before executing the query
-
-    @wraps(func)
-    async def wrapper(*args, **kwargs):
-        if not prisma.is_connected():
-            await connect_prisma()
-        try:
-            result = await func(*args, **kwargs)
-            return result
-        except Exception as e:
-            logging.error(f"Error in query: {e}")
-            raise
-
-    return wrapper
- """
+async def disconnect_db():
+    """Disconenct from the prisma database."""
+    await db.disconnect()
