cd25957d8e0bf7c1326d582f8d0d606bbbd29344
danishkodemonkey <humberjini@hotmail.com>
2025-02-25 15:12:45 +0100

    Update users pydantic validation and routers to adhere to new prisma model

--- app/routes/users.py
+++ app/routes/users.py
@@ -9,7 +9,7 @@ router = APIRouter()
 @router.get("")
 async def get_users():
     """Retrieve a list of all users."""
-    users = await db.users.find_many()
+    users = await db.user.find_many()
     return users
 
 
@@ -18,19 +18,19 @@ async def get_users():
 )  # Response must match pydantic response
 async def create_user(user: UserCreate):  # Request must match pydantic UserCreate shape
     """API-level validation, if failed will not reach prisma, saves DB calls"""
-    existing = db.users.find_unique({"email": user.email})
+    existing = await db.user.find_unique({"email": user.email})
     if existing:
         raise HTTPException(status_code=400, detail="Email already in use")
 
     """Create new user with provided data"""
-    new_user = await db.users.create(data=user.model_dump())
-    return {"message": "User created!", "data": user.model_dump()}
+    new_user = await db.user.create(data=user.model_dump())
+    return new_user
 
 
 @router.get("/{user_id}")
 async def get_user(user_id: int):
     """Retrieve a user by ID."""
-    user = await db.users.find_unique(where={"id": user_id})
+    user = await db.user.find_unique(where={"id": user_id})
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return user  # Return prisma model no conversion
--- app/schemas/users.py
+++ app/schemas/users.py
@@ -1,19 +1,31 @@
 from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
+from datetime import datetime
+from uuid import UUID
 
 
 # Input validation
 class UserCreate(BaseModel):  # Extend pydantic basemodel for validation
     email: EmailStr
-    name: str = Field(
-        ..., min_length=4, max_length=30, regex="^[a-zA-Z ]+$"
+    username: str = Field(
+        ..., min_length=4, max_length=30, pattern="^[a-zA-Z ]+$"
     )  # Regex limited to letters & spaces
+    password: Optional[str]  # Optional for Oauth users
+    oauth_provider: str  # oAuth provider (google, discord, manual)
+    oauth_id: Optional[str]  # Optional oAuth ID
+
+    class Config:
+        # Ensure values can be passed through ORM PRisma client
+        orm_mode: True
 
 
 # Output validation (responses)
 class UserResponse(BaseModel):
-    id: int
-    email: EmailStr
-    name: str
+    id: UUID  # Accomidate UUID type
+    username: str
+    email: str
+    created_at: datetime
+    updated_at: datetime
 
     class Config:
-        from_attributes = True
+        from_attributes = True  # Map prisma model attributes to pydantic fields
