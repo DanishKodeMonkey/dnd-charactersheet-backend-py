e0576f87894c0f93ddcc690d1fe2fac23bf39fc0
danishkodemonkey <humberjini@hotmail.com>
2025-02-25 15:43:38 +0100

    add auth route to router

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from .users import router as users_router
 from .characters import router as characters_router
+from .auth import router as auth_router
 
 
 def register_routers(app: FastAPI) -> None:
     app.include_router(users_router, prefix="/users", tags=["Users"])
     app.include_router(characters_router, prefix="/characters", tags=["Characters"])
+    app.include_router(auth_router, prefix="/auth", tags=["Auth"])
