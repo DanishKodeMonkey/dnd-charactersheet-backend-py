168221c32313e92b9960202f72eb8e2d84df1134
danishkodemonkey <humberjini@hotmail.com>
2025-03-12 14:50:37 +0100

    Use bcrypt directly as passlib is not maintained properly anymore

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,13 +1,15 @@
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+import bcrypt
 
 
 def hash_password(password: str) -> str:
     """Hashes password"""
-    return pwd_context.hash(password)
+    salt = bcrypt.gensalt()  # Generate salt
+    hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
+    return hashed_password.decode("utf-8")  # Return hashed password as a string
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     """Verify password"""
-    return pwd_context.verify(plain_password, hashed_password)
+    return bcrypt.checkpw(
+        plain_password.encode("utf-8"), hashed_password.encode("utf-8")
+    )
