a535966657d269993f2e95f77f816155869b8d14
danishkodemonkey <humberjini@hotmail.com>
2025-03-19 15:24:50 +0100

    Add environment reference

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -9,6 +9,7 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"  # Default algorithmn HS256
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30  # default 30
     REFRESH_TOKEN_EXPIRE_HOURS: int = 24  # default 24
+    ENVIRONMENT: str = "development"
 
     class Config:  # Configuration of Settings class
         env_file = ".env"  # env source for Settings class
