a8cbee5b26130b1e3468eedc04b4f131fefa590e
danishkodemonkey <humberjini@hotmail.com>
2025-03-27 15:24:24 +0100

    refactor to properly initialise via main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Entrypoint, app initializer
-
+import uvicorn
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
